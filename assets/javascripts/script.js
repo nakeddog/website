8c6224fc12924c33ac7c7d66eaef17f0eb07550a
Travis Jefferson <traviscjefferson@gmail.com>
2017-09-16 15:16:34 -0500

    Add images to gallery, add masonry layout
    Link in stuff for modal/lightbox gallery but don't activate that stuff
    yet

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,5 +1,25 @@
-$(document).ready(function () {
-  $(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
+$(document).ready(function() {
+  $('.navbar-nav li a').click(function(event) {
+    $('.navbar-collapse').collapse('hide');
   });
+
+  var $grid = $('.grid');
+  $grid.imagesLoaded(function() {
+    $grid.masonry({
+      // use outer width of grid-sizer for columnWidth
+      columnWidth: '.grid-sizer',
+      // do not use .grid-sizer in layout
+      itemSelector: '.grid-item',
+      gutter: '.gutter-sizer',
+      percentPosition: true
+    });
+  });
+
+  // Not sure we want this yet, but it's here for when we do
+  // $('[data-toggle="lightbox"]').click(function(event) {
+  //   event.preventDefault();
+  //   $(this).ekkoLightbox({
+  //     alwaysShowClose: true
+  //   });
+  // });
 });
